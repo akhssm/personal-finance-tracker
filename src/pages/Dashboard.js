3f1ed9f081cb583c853fa8959db7b5734af9c056
Akshay <mandaakshay20@gmail.com>
2026-01-13 12:14:22 +0530

    Completed cards page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,46 @@
-import React from 'react'
-import Header from '../components/Header'
+import React, { useState } from 'react';
+import Header from '../components/Header';
+import Cards from '../components/Cards';
+import { Modal } from 'antd';
 
 const Dashboard = () => {
+  const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
+  const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+
   return (
     <div>
-      <Header/>
-    </div>
-  )
-}
+      <Header />
 
-export default Dashboard
+      <Cards
+        showExpenseModal={() => setIsExpenseModalVisible(true)}
+        showIncomeModal={() => setIsIncomeModalVisible(true)}
+      />
+
+      {/* Income Modal */}
+      <Modal
+        style={{ fontWeight: 600 }}
+        title="Add Income"
+        open={isIncomeModalVisible}
+        onCancel={() => setIsIncomeModalVisible(false)}
+        footer={null}
+        centered
+      >
+        Income
+      </Modal>
+
+      {/* Expense Modal */}
+      <Modal
+        style={{ fontWeight: 600 }}
+        title="Add Expense"
+        open={isExpenseModalVisible}
+        onCancel={() => setIsExpenseModalVisible(false)}
+        footer={null}
+        centered
+      >
+        Expense
+      </Modal>
+    </div>
+  );
+};
+
+export default Dashboard;
