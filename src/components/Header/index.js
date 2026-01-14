33796f3e68b0c5ef9747f7b36c1975a61ccca0ac
Akshay <mandaakshay20@gmail.com>
2026-01-14 12:13:24 +0530

    Completed creating profile picture

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { signOut } from 'firebase/auth';
+import userImg from '../../assets/user.jpg';
 
 function Header() {
   const [user, loading] = useAuthState(auth);
@@ -37,7 +38,13 @@ function Header() {
     <div className="navbar">
       <p className='logo'>Financely.</p>
       {user && (
+      <div style={{ display: "flex", alignItems: "center", gap: "0.75rem" }}>
+        <img 
+          src={user.photoURL ? user.photoURL : userImg}
+          style={{ borderRadius: "50%", height: "1.5rem", width: "1.5rem" }}
+        />
         <p className='logo link' onClick={logoutFnc}>Logout</p>
+      </div>
       )}
     </div>
   );
